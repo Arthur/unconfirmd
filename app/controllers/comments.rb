9ea11a189336409f9111ef7e13bd5444e242b456
mashpit <mashpit@5662c703-82ec-43e1-b4bd-38f1f9b4df1e>
2008-05-18 13:12:04 +0000

    Resource :comments
    git-svn-id: svn://paws.bearstech.com/mashpit/unconfirmd/trunk@22 5662c703-82ec-43e1-b4bd-38f1f9b4df1e

---
+++ app/controllers/comments.rb
@@ -0,0 +1,58 @@
+class Comments < Application
+  # provides :xml, :yaml, :js
+
+  def index
+    @comments = Comment.all
+    display @comments
+  end
+
+  def show
+    @comment = Comment.get()
+    raise NotFound unless @comment
+    display @comment
+  end
+
+  def new
+    only_provides :html
+    @comment = Comment.new
+    render
+  end
+
+  def edit
+    only_provides :html
+    @comment = Comment.get()
+    raise NotFound unless @comment
+    render
+  end
+
+  def create
+    @comment = Comment.new(params[:comment])
+    if @comment.save
+      redirect url(:comment, @comment)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @comment = Comment.get()
+    raise NotFound unless @comment
+    @comment.attributes = params[:comment]
+    if  @comment.save
+      redirect url(:comment, @comment)
+    else
+      raise BadRequest
+    end
+  end
+
+  def destroy
+    @comment = Comment.get()
+    raise NotFound unless @comment
+    if @comment.destroy
+      redirect url(:comment)
+    else
+      raise BadRequest
+    end
+  end
+
+end
