34a888d124a0ad2c4b50bf4e6b3949e30a3704fd
mashpit <mashpit@5662c703-82ec-43e1-b4bd-38f1f9b4df1e>
2008-05-18 13:12:24 +0000

    commit some shit
    git-svn-id: svn://paws.bearstech.com/mashpit/unconfirmd/trunk@23 5662c703-82ec-43e1-b4bd-38f1f9b4df1e

--- config/router.rb
+++ config/router.rb
@@ -33,5 +33,5 @@ Merb::Router.prepare do |r|
   r.default_routes
   
   # Change this for your home page to be available at /
-  # r.match('/').to(:controller => 'whatever', :action =>'index')
+  r.match('/').to(:controller => 'rumors', :action =>'index')
 end
